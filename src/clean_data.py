a774994d232267938851c573a61c146c8587ed1d
Daniel Chockler <daniel.chockler@gmail.com>
2025-11-01 15:53:51 +0000

    Added month to datasets

--- src/clean_data.py
+++ src/clean_data.py
@@ -3,10 +3,10 @@ import os
 
 class CreateDataSet:
 
-    def __init__(self, input_file, output_dir):
+    def __init__(self, input_file, output_dir, columns):
         self.file_path = input_file
         self.output_dir = output_dir
-        self.columns = ['stateProvince', 'year', 'decimalLatitude', 'decimalLongitude']
+        self.columns = columns
         
         self.raw_df = None
         self.processed_data = None
@@ -16,7 +16,7 @@ class CreateDataSet:
     def read_dataset(self):
         try:
             print(f"Reading file: {self.file_path}")
-            self.raw_df = pd.read_csv(self.file_path, sep='\t', usecols=self.columns, ow_memory=False)
+            self.raw_df = pd.read_csv(self.file_path, sep='\t', usecols=self.columns)
 
         except Exception as e:
             print(f"Failed to read dataset: {e}")
@@ -29,11 +29,12 @@ class CreateDataSet:
         df_filtered = self.raw_df[self.raw_df['stateProvince'] == 'Pennsylvania'].copy()
         df_filtered.dropna(subset=['year', 'decimalLatitude', 'decimalLongitude'], inplace=True)
         df_filtered['year'] = df_filtered['year'].astype(int)
+        df_filtered['month'] = df_filtered['month'].astype(int)
 
         df_filtered['latitudeGrid'] = df_filtered['decimalLatitude'].round(1)
         df_filtered['longitudeGrid'] = df_filtered['decimalLongitude'].round(1)
 
-        self.processed_data = df_filtered.groupby(['latitudeGrid', 'longitudeGrid', 'year']).size().reset_index(name='sightingCount')
+        self.processed_data = df_filtered.groupby(['latitudeGrid', 'longitudeGrid', 'year', 'month']).size().reset_index(name='sightingCount')
         
     def save_data_by_year(self, beginning_year=2018):
         if self.processed_data is None:
@@ -57,9 +58,10 @@ if __name__ == "__main__":
 
     INPUT_FILE = '../Dataset/dirtyData/0010762-251025141854904.csv'
     OUTPUT_FOLDER = '../Dataset/cleanData'
+    COLUMNS = ['stateProvince', 'year', 'month', 'decimalLatitude', 'decimalLongitude']
 
     try:
-        data_processor = CreateDataSet(input_file=INPUT_FILE, output_dir=OUTPUT_FOLDER)
+        data_processor = CreateDataSet(input_file=INPUT_FILE, output_dir=OUTPUT_FOLDER, columns=COLUMNS)
         
         data_processor.read_dataset()
         data_processor.process_data()
